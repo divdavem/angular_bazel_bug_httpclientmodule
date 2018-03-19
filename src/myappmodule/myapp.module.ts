b5d8a539a0fc53dec571fdf95cdb2947437894c4
divdavem <david-emmanuel.divernois@amadeus.com>
2018-03-19 15:25:18 +0100

    Adding dependency on HttpClientModule: ts_devserver no longer works

--- src/myappmodule/myapp.module.ts
+++ src/myappmodule/myapp.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { MyComponent } from './mycomponent';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        HttpClientModule
     ],
     declarations: [MyComponent],
     bootstrap: [MyComponent]
